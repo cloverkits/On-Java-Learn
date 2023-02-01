ff2966d33233327cb963d0d59e4ab5d52e232bf0
Cloverkit <cloverkit@outlook.com>
2023-02-01 20:29:16 +0800

    Init commit

---
+++ src/main/java/ml/cloverkit/objects/HelloDate.java
@@ -0,0 +1,11 @@
+package ml.cloverkit.objects;
+
+import java.util.Date;
+
+public class HelloDate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
+}
