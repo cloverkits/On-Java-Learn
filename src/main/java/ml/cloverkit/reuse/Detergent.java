d7b3144dc61fe1995f9be0ead6dd62b4f62482fd
Cloverkit <cloverkit@outlook.com>
2023-02-08 22:14:00 +0800

    P181

---
+++ src/main/java/ml/cloverkit/reuse/Detergent.java
@@ -0,0 +1,60 @@
+package ml.cloverkit.reuse;
+
+class Cleanser {
+    private String s = "Cleanser";
+
+    public void append(String a) {
+        s += a;
+    }
+
+    public void dilute() {
+        append(" dilute()");
+    }
+
+    public void apply() {
+        append(" apply");
+    }
+
+    public void scrub() {
+        append(" scrub()");
+    }
+
+    @Override
+    public String toString() {
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Cleanser x = new Cleanser();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        System.out.println(x);
+    }
+}
+
+public class Detergent extends Cleanser {
+    // 修改方法
+    @Override
+    public void scrub() {
+        append(" Detergent.scrub()");
+        // 调用基类方法
+        super.scrub();
+    }
+
+    // 添加方法
+    public void foam() {
+        append(" foam()");
+    }
+
+    public static void main(String[] args) {
+        Detergent x = new Detergent();
+        x.dilute();
+        x.apply();
+        x.scrub();
+        x.foam();
+        System.out.println(x);
+        System.out.println("Testing base cass:");
+        Cleanser.main(args);
+    }
+}
