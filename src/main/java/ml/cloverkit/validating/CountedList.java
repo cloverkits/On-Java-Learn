32e6f9af7a45b9b49ac2f49d229a3e5953db4c06
cloverkit <cloverkit@outlook.com>
2023-05-09 21:59:19 +0800

    P480

---
+++ src/main/java/ml/cloverkit/validating/CountedList.java
@@ -0,0 +1,18 @@
+package ml.cloverkit.validating;
+
+// 跟踪自身创建的数量
+
+import java.util.ArrayList;
+
+public class CountedList extends ArrayList<String> {
+    private static int counter = 0;
+    private int id = counter++;
+
+    public CountedList() {
+        System.out.println("CountedList #" + id);
+    }
+
+    public int getId() {
+        return id;
+    }
+}
